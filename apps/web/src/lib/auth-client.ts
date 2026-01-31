eab161f66ea521d0f4912c0ebe1f7c610cbcdf25
Shaik Muzakkir Hussain <sk.muzakkirhussain@gmail.com>
2026-02-01 04:39:30 +0530

    feat: Initialize full-stack monorepo project with TanStack Start, Elysia, oRPC, Drizzle, and shadcn/ui components.

---
+++ apps/web/src/lib/auth-client.ts
@@ -0,0 +1,6 @@
+import { env } from "@elysia-demo/env/web";
+import { createAuthClient } from "better-auth/react";
+
+export const authClient = createAuthClient({
+  baseURL: env.NEXT_PUBLIC_SERVER_URL,
+});
---
+++ packages/auth/src/index.ts
@@ -0,0 +1,25 @@
+import { db } from "@elysia-demo/db";
+import * as schema from "@elysia-demo/db/schema/auth";
+import { env } from "@elysia-demo/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+  },
+  plugins: [],
+});
